a48caaf88251feb363dd4b709eb4796d74e2b859
Austin Graham <austiingraham@gmail.com>
2023-10-20 04:23:02 -0400

    added missing semicolon

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ function generatePassword() {
   var passwordLength = prompt ("How many characters would you like your password to be? Enter a number between 8-128.");
 
   if (passwordLength < 8 || passwordLength > 128) {
-    alert ("The password must be at least 8 characters and no more than 128 characters. Enter a number between 8-128.")
+    alert ("The password must be at least 8 characters and no more than 128 characters. Enter a number between 8-128.");
     return "";
   }
 }
