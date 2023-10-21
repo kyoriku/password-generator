3ff4ade5ad194f4c052551a25449a5eb37273c21
Austin Graham <austiingraham@gmail.com>
2023-10-21 02:32:47 -0400

    added confirm box for uppercase characters, added links in README file

---
+++ README.md
@@ -0,0 +1,18 @@
+# JavaScript Password Generator
+
+## Description
+- 
+
+## Usage
+-
+
+## Credits
+- [MDN Web Docs - Window: prompt() method](https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt)
+- [MDN Web Docs - Window: alert() method](https://developer.mozilla.org/en-US/docs/Web/API/Window/alert)
+- [MDN Web Docs - Window: confirm() method](https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm)
+- [MDN Web Docs - Logical OR (||)](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_OR)
+- [MDN Web Docs - return](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return)
+- [MDN Web Docs - String.prototype.concat()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/concat)
+
+## License
+Please refer to the LICENSE in the repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ function generatePassword() {
   var specialCharacters = "!#$%&'()*+,-./:;<=>?@][^_`{|}~";
   var includedCharacterType = "";
 
-  var passwordLength = prompt ("How many characters would you like your password to be? Enter a number between 8-128.");
+  var passwordLength = prompt ("How many characters do you want the password to be? Enter a number between 8-128.");
 
   if (passwordLength < 8 || passwordLength > 128) {
     alert ("The password must be at least 8 characters and no more than 128 characters. Enter a number between 8-128.");
@@ -32,5 +32,11 @@ function generatePassword() {
 
   if (includeLowercaseCharacters) {
     includedCharacterType = includedCharacterType.concat(lowercaseCharacters)
+  }
+
+  var includeUppercaseCharacters = confirm ("Do you want to include uppercase characters?")
+
+  if (includeUppercaseCharacters) {
+    includedCharacterType = includedCharacterType.concat(uppercaseCharacters)
   }
 }
